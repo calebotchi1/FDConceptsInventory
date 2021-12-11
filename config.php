884a34a58b4f8d3b2e0f650997a70f5bb63ab75f
calebotchi1 <46813609+calebotchi1@users.noreply.github.com>
2021-12-11 23:56:43 +0000

    changes

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', '51.140.122.0', 'earlybunny3', 'fdc_inventory');
+$db = mysqli_connect('51.140.122.0', 'root', 'earlybunny3', 'fdc_inventory');
 if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
